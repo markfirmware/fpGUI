4305820c5248700caedbb84522997e348372a656
markfirmware <markfirmware@users.noreply.github.com>
2017-01-24 21:57:14 -0500

    Update appveyor-build-fpgui.sh

--- scripts/appveyor-build-fpgui.sh
+++ scripts/appveyor-build-fpgui.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-cd src
-cmd //c build_ultibo.bat
-cd ../examples/corelib/aggcanvas
-mkdir units
-cmd //c build_ultibo.bat
-cd ../canvastest
-cmd //c build_ultibo.bat
-cd ../helloworld
-cmd //c build_ultibo.bat
+
+BUILD_OUTPUT=$(pwd)/build-output
+LOG=$BUILD_OUTPUT/build.log
+
+function build {
+    cd src
+    cmd //c build_ultibo.bat
+    cd ../examples/corelib/aggcanvas
+    mkdir units
+    cmd //c build_ultibo.bat
+    cd ../canvastest
+    cmd //c build_ultibo.bat
+    cd ../helloworld
+    cmd //c build_ultibo.bat
+    cd ../..
+}
+
+build |& tee $LOG
